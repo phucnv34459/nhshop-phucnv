9fbf8d9d69a312c2ea572e455459449aac6a3baf
phucnv34459 <phucvan050004@gmail.com>
2024-06-04 10:34:43 +0700

    buổi1-banner

--- tailwind.config.js
+++ tailwind.config.js
@@ -1,8 +1,20 @@
 /** @type {import('tailwindcss').Config} */
 export default {
-  content: ["./index.html", "./src/**/*.html"],
+  content: ["./index.html", "./src/**/*.{js,ts,jsx,tsx}"],
   theme: {
-      extend: {},
+      extend: {
+          colors: {
+              primary: "#17AF26",
+              secondary: "#05422C",
+              "neutral-400": "#F4F4F4",
+          },
+          backgroundImage: {
+              hero: "url('./assets/images/banner-desktop.jpg')",
+          },
+          backgroundSize: {
+              "50%": "50%",
+          },
+      },
   },
   plugins: [],
 };
